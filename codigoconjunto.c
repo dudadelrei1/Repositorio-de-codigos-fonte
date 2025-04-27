fffa1426ed3c23fec87c72e1af53d3e5b323b4bd
felipeazeve1 <felipeazeve1@outlook.com>
2025-04-27 01:08:24 -0300

    Teste

--- codigoconjunto.c
+++ codigoconjunto.c
@@ -3,6 +3,6 @@
 int main(){
     printf("Duda Del Rei passou por aqui!\n");
     printf("Laura Luiza esteve aqui\n");
-    printf("Felipe esteve por aqui!\n");
+    printf("Felipe Azevedo esteve por aqui!\n");
     printf("Paulo Sergio esteve por aqui\n");
 }
